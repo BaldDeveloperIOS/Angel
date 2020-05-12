b772b15297511e149d6e3147917e8230a6bb26b8
BaldDeveloperIOS <thebalddeveloper@gmail.com>
2020-05-12 10:49:20 +0200

    Bloc Keepers du profil ajouté.

--- Angel/KeeperProfile.swift
+++ Angel/KeeperProfile.swift
@@ -10,9 +10,68 @@ import SwiftUI
 
 struct KeeperProfile: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            
+            RoundedRectangle(cornerRadius: 25)
+                .foregroundColor(Color("ProfileColor"))
+                .frame(width: 380, height: 330)
+                .opacity(0.4)
+                .shadow(color: .gray, radius: 5, x: 10, y: 10)
+            
+            VStack {
+                
+                // Stack Keepers
+                VStack  (alignment: .leading){
+                    Text("Mes gardiens")
+                        .font(.title)
+                    Spacer().frame(height: 0)
+                    // Stack photos Keepers
+                    HStack {
+                        Image("KeeperOne").resizable()
+                            .frame(width: 90, height: 90)
+                            .clipShape(Circle())
+                            .padding(.trailing, 10)
+                        Image("KeeperTwo").resizable()
+                            .frame(width: 90, height: 90)
+                            .clipShape(Circle())
+                            .padding(.trailing, 10)
+                        Image("KeeperThree").resizable()
+                            .frame(width: 90, height: 90)
+                            .clipShape(Circle())
+                    }.padding()
+                }
+                Spacer().frame(height: 0)
+                // Stack Protégés
+                VStack  (alignment: .leading){
+                    Text("Mes protégés")
+                        .font(.title)
+                    Spacer().frame(height: 0)
+                    // Stack photos Protégés
+                    HStack {
+                        Image("protégéOne").resizable()
+                            .frame(width: 90, height: 90)
+                            .clipShape(Circle())
+                            .padding(.trailing, 10)
+                        Image("protégéTwo").resizable()
+                            .frame(width: 90, height: 90)
+                            .clipShape(Circle())
+                            .padding(.trailing, 10)
+                        ZStack {
+                        Circle()
+                            .foregroundColor(.gray)
+                            .frame(width: 90, height: 90)
+                        Image(systemName: "plus")
+                            .font(.title)
+                            .foregroundColor(.white)
+                        }
+                    }.padding()
+                }
+            }
+        }
     }
 }
+
 
 struct KeeperProfile_Previews: PreviewProvider {
     static var previews: some View {
---
+++ Angel/ProfileButtons.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileButtons.swift
+//  Angel
+//
+//  Created by chauveau on 12/05/2020.
+//  Copyright © 2020 chauveau. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProfileButtons: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileButtons_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileButtons()
+    }
+}
