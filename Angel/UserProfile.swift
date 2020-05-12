76f6067edc09a18917b2059d620201c1c83115c4
BaldDeveloperIOS <thebalddeveloper@gmail.com>
2020-05-12 10:07:40 +0200

    UserProfile complete.

--- Angel/UserProfile.swift
+++ Angel/UserProfile.swift
@@ -11,6 +11,8 @@ import SwiftUI
 struct UserProfile: View {
     var body: some View {
         
+        ZStack (alignment: .leading){
+            
         Rectangle()
             .foregroundColor(.clear)
             .background(LinearGradient(gradient:  Gradient(colors: [.white, Color("PurpleAngel")]), startPoint: .top, endPoint: .bottom))
@@ -18,8 +20,30 @@ struct UserProfile: View {
             .frame(width: 380, height: 160)
             .opacity(0.8)
             .shadow(color: .gray, radius: 5, x: 10, y: 10)
-        
-        
+          
+            HStack (alignment: .top){
+            Image("portrait").resizable()
+                .frame(width: 120, height: 120)
+                .clipShape(Circle())
+                .padding()
+                
+                VStack (alignment: .leading){
+                    Text("Ana Tolie")
+                        .font(.system(size: 35))
+                        .fontWeight(.bold)
+                        .offset(y: 10)
+                    
+                   Spacer().frame(height: 45)
+                    
+                    VStack (alignment: .leading){
+                    Text("06 17 34 52 49")
+                    .font(.system(size: 20))
+                    Text("anatolie@gmail.com")
+                    .font(.system(size: 20))
+                    }
+                }
+            }
+        }
     }
 }
 
