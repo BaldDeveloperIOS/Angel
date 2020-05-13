a59ebad8304033de586f4aa344de1de5818113de
Nkabura <n.venantie@yahoo.fr>
2020-05-13 10:49:35 +0200

    Modification button, ajouter une photo, ajouter screenshot

--- Angel/ContentView.swift
+++ Angel/ContentView.swift
@@ -8,6 +8,7 @@
 
 import SwiftUI
 let lightGreyColor=Color(red:239/255,green:243/255,blue:244/255,opacity: 1.0)
+extension Color{ static let PulpeAngel = Color(red:88/255,green:86/255,blue:214/255)}
 struct ContentView: View {
     @State var username: String=""
     @State var userFastname: String=""
@@ -47,9 +48,41 @@ struct ContentView: View {
             .background(lightGreyColor)
             .cornerRadius(35.0)
             .padding(.bottom,20)
-                Toggle("Ajouter une photo",isOn: $addPhoto).padding()
-                Toggle("Ajouter screenshot",isOn: $addScreenShot).padding()
-            
+            HStack{
+        Text("Ajouter une photo")
+                .font(.headline)
+                .foregroundColor(.black)
+                .padding()
+                    Button(action:{print("Button taper")})
+                                {
+                                   Text("")
+                                       .font(.headline)
+                                       .foregroundColor(.black)
+                                       .padding()
+                                       .frame(width:150, height:40)
+                                       .background(Color.PulpeAngel)
+                                       .cornerRadius(35.0)
+                               }.padding()
+        
+            }
+            HStack{
+                   Text("Ajouter screenshot")
+                           .font(.headline)
+                           .foregroundColor(.black)
+                           .padding()
+                               Button(action:{print("Button taper")})
+                                           {
+                                              Text("")
+                                                  .font(.headline)
+                                                  .foregroundColor(.black)
+                                                  .padding()
+                                                  .frame(width:150, height:40)
+                                                  .background(Color.PulpeAngel)
+                                                  .cornerRadius(35.0)
+                                          }.padding()
+                   
+                       }
+          
              Button(action:{print("Button taper")})
              {
                 Text("AJOUTER MES CONTACTS")
@@ -57,7 +90,7 @@ struct ContentView: View {
                     .foregroundColor(.black)
                     .padding()
                     .frame(width:350, height:40)
-                    .background(Color.blue)
+                    .background(Color.PulpeAngel)
                     .cornerRadius(35.0)
             }.padding()
             Button(action:{print("Button taper")})
@@ -67,7 +100,7 @@ struct ContentView: View {
                 .foregroundColor(.black)
                 .padding()
                 .frame(width: 350, height: 40)
-                .background(Color.blue)
+                .background(Color.PulpeAngel)
                 .cornerRadius(35.0)
         }
     }
@@ -87,7 +120,7 @@ struct ExtractedView: View {
         Text("Mon profil")
             .font(.largeTitle)
             .fontWeight(.semibold)
-            .foregroundColor(.blue)
+            .foregroundColor(.PulpeAngel)
             .padding(.bottom,20)
     }
 }
