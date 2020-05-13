82a452f2a00bd8c8a95176b566d678884b471a3b
BaldDeveloperIOS <thebalddeveloper@gmail.com>
2020-05-13 11:27:32 +0200

    Re-Indent Conexxion.

--- Angel/Connexion.swift
+++ Angel/Connexion.swift
@@ -14,57 +14,57 @@ struct Connexion: View {
     @State private var isvalid = false
     
     var body: some View {
-          
-                VStack {
-                   
-                    Spacer()
-                        .padding()
-                    TextField("login", text: $login)
-                   
-                        .padding()
-                                 .background(Color.white)
-                                 .overlay(RoundedRectangle(cornerRadius: 18).stroke(Color.PurpleAngel, lineWidth: 2)
-                             .foregroundColor(.clear))
-                             .padding(15)
-                    SecureField("password",text: $password){
-                        self.ispasswordvalid()
-                    } .padding()
-                                                    .background(Color.white)
-                                                    .overlay(RoundedRectangle(cornerRadius: 18).stroke(Color.PurpleAngel, lineWidth: 2)
-                                                .foregroundColor(.clear))
-                                                .padding(15)
-                    if !isvalid {
-                        Text("").foregroundColor(.PurpleAngel)
-                        
-                        
-                        Text("MOT DE PASSE OUBLIÉ")
-                         .foregroundColor(Color.PurpleAngel)
-                    }
-                    Spacer().frame(height:50)
-                    Spacer()
-             Button(action: {
-
-                           
-                       })  {
-                           Text("Connexion").font(.title).bold()
-                               .padding(12.0)
-                               .frame(width: 357.0)
-                               .background(Color.PurpleAngel)
-                           .foregroundColor(.white)
-                               .cornerRadius(20.0)
-                       }.padding().frame(width: 400.0, height: 50.0)
-                    Spacer()
-                        .padding()
-                    Spacer()
-                                   Image("Logo")
-                                       .resizable()
-                                       .foregroundColor(.PurpleAngel)
-                                   .frame(width:400.0, height:400.0 )
-                                   .padding(1.0)
-                    
-                }
-          
-           
+        
+        VStack {
+            
+            Spacer()
+                .padding()
+            TextField("login", text: $login)
+                
+                .padding()
+                .background(Color.white)
+                .overlay(RoundedRectangle(cornerRadius: 18).stroke(Color.PurpleAngel, lineWidth: 2)
+                    .foregroundColor(.clear))
+                .padding(15)
+            SecureField("password",text: $password){
+                self.ispasswordvalid()
+            } .padding()
+                .background(Color.white)
+                .overlay(RoundedRectangle(cornerRadius: 18).stroke(Color.PurpleAngel, lineWidth: 2)
+                    .foregroundColor(.clear))
+                .padding(15)
+            if !isvalid {
+                Text("").foregroundColor(.PurpleAngel)
+                
+                
+                Text("MOT DE PASSE OUBLIÉ")
+                    .foregroundColor(Color.PurpleAngel)
+            }
+            Spacer().frame(height:50)
+            Spacer()
+            Button(action: {
+                
+                
+            })  {
+                Text("Connexion").font(.title).bold()
+                    .padding(12.0)
+                    .frame(width: 357.0)
+                    .background(Color.PurpleAngel)
+                    .foregroundColor(.white)
+                    .cornerRadius(20.0)
+            }.padding().frame(width: 400.0, height: 50.0)
+            Spacer()
+                .padding()
+            Spacer()
+            Image("Logo")
+                .resizable()
+                .foregroundColor(.PurpleAngel)
+                .frame(width:400.0, height:400.0 )
+                .padding(1.0)
+            
+        }
+        
+        
     }
     
     func ispasswordvalid() {
@@ -74,9 +74,9 @@ struct Connexion: View {
             self.isvalid = false
         }
     }
-struct Connexion_Previews: PreviewProvider {
-    static var previews: some View {
-       Connexion()
+    struct Connexion_Previews: PreviewProvider {
+        static var previews: some View {
+            Connexion()
         }
-}
+    }
 }
