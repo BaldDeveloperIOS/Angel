b2ac59d2b949b91316136c6b4fb2cad84941139d
BaldDeveloperIOS <thebalddeveloper@gmail.com>
2020-05-13 09:55:13 +0200

    Add font et images pour OnBarding.

---
+++ Angel/Subview.swift
@@ -0,0 +1,49 @@
+//
+//  Subview.swift
+//  Angel
+//
+//  Created by chauveau on 12/05/2020.
+//  Copyright © 2020 chauveau. All rights reserved.
+//
+
+import SwiftUI
+
+struct Subview: View {
+    var imageString : String
+    
+    var body: some View {
+        VStack {
+           Image(imageString).resizable()
+           .frame(width: 150, height: 150)
+            
+            
+            
+            
+            
+            /*
+            VStack {
+        
+            Text("''Ton ange gardien digital''")
+               .font(.custom("Roboto-Light", size: 28))
+            }
+            
+            Image("inscription").resizable()
+            .frame(width: 60, height: 60)
+            .rotationEffect(.degrees(-90))
+            
+            
+           Image("pictoOne").resizable()
+            .frame(width: 100, height: 100)
+            .rotationEffect(.degrees(-90))
+            */
+            
+            
+        }
+    }
+}
+
+struct Subview_Previews: PreviewProvider {
+    static var previews: some View {
+        Subview(imageString: "Logo")
+    }
+}
